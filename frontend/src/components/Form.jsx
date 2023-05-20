49fa9eec274ca80f9b7f19067d24d55e79919b1e
pmruthyu <pmruthyu@students.cs.ubc.ca>
2023-05-20 16:03:45 -0700

    Updated package.json

---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,18 @@
+
+const Form = () => {
+
+    return (
+        <div className="form">
+            <textarea
+                rows="5"
+                className="form"
+                placeholder="Enter the details of your trip"
+            ></textarea>
+            <button className="btn">
+                Generate Itenary 
+            </button>
+        </div>
+    )
+}
+
+export default Form;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import HomePage from './pages/Home';
 import Test from './pages/Test'
+import Plan from './pages/Plan'
+
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <ChakraProvider>
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/test" element={<Test/>} />
+        <Route path="/plan" element={<Plan />} />
       </Routes>
   </Router>
+  </ChakraProvider>
 );
